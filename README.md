3b4ecc7cfc68a17ed7fa484564d0a1e1b6b9cdfa
Chamoda Deshan <chamodadeshan2002@gmail.com>
2026-09-14 04:28:15 +0530

    requirements, and setup files for project initialization

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # medical-rag-chatbot-using-LLMs-LangChain-Pinecone-Flask-AWS
+
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Chamoda Deshan",
+    author_email="chamodadeshan2002@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+    
+)
